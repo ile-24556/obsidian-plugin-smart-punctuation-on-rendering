5df6f34d55caae2ab5b1b1c76ec8b45295c07dfd
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-05-08 15:45:33 +0900

    Convert punctuations in US English

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,58 @@
 import { Plugin } from "obsidian";
 
-const ALL_PUNCTUATIONS: Record<string, string> = {
-  "---": "\u2014",
-  "--": "\u2013",
-};
+interface Pattern {
+  char: string;
+  pattern: RegExp;
+  replacement: string;
+}
+
+const ALL_PUNCTUATIONS: Pattern[] = [
+  {
+    char: "en dash",
+    pattern: /(^|[^-])--([^-]|$)/g,
+    replacement: "$1\u2013$2",
+  },
+  {
+    char: "em dash",
+    pattern: /(^|[^-])---([^-]|$)/g,
+    replacement: "$1\u2014$2",
+  },
+  {
+    char: "left single quotation mark",
+    pattern: /(^|[ ])'([^ ])/g,
+    replacement: "$1\u2018$2",
+  },
+  {
+    char: "right single quotation mark",
+    pattern: /([^ ])'/g,
+    replacement: "$1\u2019",
+  },
+  {
+    char: "left double quotation mark",
+    pattern: /(^|[ ])"([^ ])/g,
+    replacement: "$1\u201C$2",
+  },
+  {
+    char: "right double quotation mark",
+    pattern: /([^ ])"/g,
+    replacement: "$1\u201D",
+  },
+  {
+    char: "horizontal ellipsis",
+    pattern: /(^|[^.])\.\.\.($|[^.])/g,
+    replacement: "$1\u2026$2",
+  },
+];
 
 export default class ExamplePlugin extends Plugin {
   override async onload() {
-    this.registerMarkdownPostProcessor((element, _context) => {
-      const paragraphs = element.findAll("p");
-
-      for (const paragraph of paragraphs) {
-        let text = paragraph.innerText;
-        for (const key in ALL_PUNCTUATIONS) {
-          const v = ALL_PUNCTUATIONS[key]!;
-          text = text.replaceAll(key, v);
+    this.registerMarkdownPostProcessor((element) => {
+      for (const child of element.children) {
+        let text = child.innerHTML;
+        for (const e of ALL_PUNCTUATIONS) {
+          text = text.replaceAll(e.pattern, e.replacement);
         }
-        paragraph.replaceWith(text);
+        child.innerHTML = text;
       }
     });
   }
