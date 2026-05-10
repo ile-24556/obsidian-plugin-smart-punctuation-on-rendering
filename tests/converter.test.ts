ebf37f6dd4793c43ade022ab082c083bf5ae293f
ile-24556 <110268848+ile-24556@users.noreply.github.com>
2026-05-10 15:13:49 +0900

    Change existing test into parameter test

--- tests/converter.test.ts
+++ tests/converter.test.ts
@@ -3,8 +3,24 @@ import { convert } from "../src/converter";
 
 describe("converter", () => {
   describe("convert()", () => {
-    test("curly apostrophe", () => {
-      expect(convert("That's it")).toBe("That’s it");
-    });
+    interface Case {
+      description: string;
+      input: string;
+      output: string;
+    }
+
+    const cases: Case[] = [
+      {
+        description: "curly apostrophe",
+        input: "That's it",
+        output: "That’s it",
+      },
+    ];
+
+    for (const c of cases) {
+      test(c.description, () => {
+        expect(convert(c.input)).toBe(c.output);
+      });
+    }
   });
 });
